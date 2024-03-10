fc45d764f7528aad3428a4af452f73538547fe49
Muhammet Enes Çetin <enespcetin@gmail.com>
2024-03-10 15:29:20 +0300

    our story page styling and performance update

--- components/Articles/ArticleWrapper.tsx
+++ components/Articles/ArticleWrapper.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@/lib/utils';
 import { Dosis } from 'next/font/google';
 import Image from 'next/image';
+import { BackgroundBeams } from '../ui/background-beams';
 
 type ArticleWrapperProps = {
   image: string;
@@ -20,11 +21,12 @@ function ArticleWrapper({
   alt,
 }: ArticleWrapperProps) {
   return (
-    <article className="flex aspect-auto flex-col gap-5 lg:flex-row my-5 items-start mx-auto">
+    <article className="flex aspect-auto flex-col gap-5 lg:flex-row my-5 items-start mx-auto relative p-10">
+      <BackgroundBeams className="bg-slate-300/10 rounded-md shadow-md" />
       <div className="flex-[2] bg-ellipse from-custom-red-100 to-custom-red-400 p-2 w-max h-max rounded-sm z-10">
-        <div className="relative w-72 sm:min-w-96 min-h-80 lg:min-w-[450px] lg:min-h-[300px] overflow-hidden cursor-pointer">
+        <div className="relative w-72 sm:min-w-96 lg:min-w-[450px] overflow-hidden cursor-pointer aspect-square">
           <Image
-            className="object-cover hover:scale-110 transition-all duration-300 user-drag-none"
+            className="absolute object-cover hover:scale-110 transition-all duration-300 user-drag-none"
             src={image}
             alt={alt}
             fill
@@ -33,10 +35,10 @@ function ArticleWrapper({
         </div>
       </div>
       <section className={cn('flex-[3] flex flex-col gap-y-5', font.className)}>
-        <h1 className="text-5xl font-medium drop-shadow-border text-white">
+        <h1 className="text-5xl font-medium drop-shadow-border text-white xl:text-6xl">
           {header}
         </h1>
-        <p className="text-lg leading-tight lg:leading-relaxed lg:text-xl drop-shadow-black-md text-white w-72 sm:w-96 xl:w-[600px] ">
+        <p className="text-lg leading-tight lg:leading-relaxed lg:text-2xl drop-shadow-black-md text-white w-72 sm:w-96 xl:w-[600px] ">
           {description}
         </p>
       </section>
